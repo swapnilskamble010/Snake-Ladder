a8a0dc45728bd9df7d75416243838222f678d398
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-14 14:46:49 +0530

    Player start position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -4,6 +4,7 @@ public class SnakeLadder {
 	
 	public static void main(String[] args) {
 		System.out.println("--Welcome to Snake and Ladder Game--\n\tLet's play..");
+		int playerPosition = 0; //start position
 	}
 
 }
