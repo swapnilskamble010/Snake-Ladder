83acb433ac8355189362782aacedb243d67257bc
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-14 15:07:58 +0530

    Dice Roll logic added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,6 +5,8 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("--Welcome to Snake and Ladder Game--\n\tLet's play..");
 		int playerPosition = 0; //start position
+		byte dice = (byte)(Math.random()*6 + 1 );
+		System.out.println("Dice Show: " + dice);
 	}
 
 }
