122ad9d22a940bd399b5a2c565bd44bf4ff9dae4
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-14 15:34:25 +0530

    Player option logic added

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -7,6 +7,30 @@ public class SnakeLadder {
 		int playerPosition = 0; //start position
 		byte dice = (byte)(Math.random()*6 + 1 );
 		System.out.println("Dice Show: " + dice);
+		byte playerOptions = (byte)(Math.random()*3 );
+		
+		switch (playerOptions) {
+		
+		case 0:
+			System.out.println("Player choose to Not to Play");
+			playerPosition = playerPosition;
+			break;
+			
+		case 1:
+			System.out.println("Player played & got a Ladder");
+			playerPosition = playerPosition + dice;
+			break;
+			
+		default:
+			System.out.println("Player played & got a Snake");
+			playerPosition = playerPosition - dice;
+			if (playerPosition <= 0) {
+				playerPosition = 0;
+			}
+			break;	
+	
+		}
+		System.out.println("Player is at position : " + playerPosition);
 	}
 
 }
