3893e5dd15ae433d1ef1e678b4c26571817ee2db
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-14 15:53:04 +0530

    Repeating till Player reach win position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -5,32 +5,38 @@ public class SnakeLadder {
 	public static void main(String[] args) {
 		System.out.println("--Welcome to Snake and Ladder Game--\n\tLet's play..");
 		int playerPosition = 0; //start position
-		byte dice = (byte)(Math.random()*6 + 1 );
-		System.out.println("Dice Show: " + dice);
-		byte playerOptions = (byte)(Math.random()*3 );
 		
-		switch (playerOptions) {
 		
-		case 0:
-			System.out.println("Player choose to Not to Play");
-			playerPosition = playerPosition;
-			break;
+		while (playerPosition < 100) {
 			
-		case 1:
-			System.out.println("Player played & got a Ladder");
-			playerPosition = playerPosition + dice;
-			break;
+			byte dice = (byte)(Math.random()*6 + 1 );
+			System.out.println("Dice Show: " + dice);
+			byte playerOptions = (byte)(Math.random()*3 );
 			
-		default:
-			System.out.println("Player played & got a Snake");
-			playerPosition = playerPosition - dice;
-			if (playerPosition <= 0) {
-				playerPosition = 0;
+			switch (playerOptions) {
+			
+			case 0:
+				System.out.println("Player choose to Not to Play");
+				break;
+				
+			case 1:
+				System.out.println("Player played & got a Ladder");
+				playerPosition = playerPosition + dice;
+				break;
+				
+			default:
+				System.out.println("Player played & got a Snake");
+				playerPosition = playerPosition - dice;
+				if (playerPosition <= 0) {
+					playerPosition = 0;
+				}
+				break;	
+		
 			}
-			break;	
-	
+			System.out.println("Player is at position : " + playerPosition);
+			
 		}
-		System.out.println("Player is at position : " + playerPosition);
+		
 	}
 
 }
